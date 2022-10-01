07c12215315c753d38a82bc389efcb6da3cae54f
MAC <mac@MACs-MacBook-Pro.local>
2022-10-01 17:05:51 -0400

    HomeScreen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,13 +1,33 @@
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
+import { Image, SafeAreaView, ScrollView, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import FitnessCard from '../components/FitnessCard'
 
 const HomeScreen = () => {
   return (
-    <SafeAreaView>
+    <ScrollView style={{marginTop:50}}>
         <View style={{backgroundColor:'#CD853f', padding:10, height:200, width:'100%'}}>
       <Text style={{color:'white', fontWeight:'bold', fontSize:18}}>HomeScreen</Text>
+      <View style={{flexDirection:'row', alignItems:'center', justifyContent:'space-between', marginTop:20}}>
+        <View>
+            <Text style={{textAlign:'center', fontWeight:'bold', color:'white' , fontSize:18}}>0</Text>
+            <Text style={{color:'gray', fontSize:17, marginTop: 6}} >WORKOUTS</Text>
+        </View>
+        <View>
+            <Text style={{textAlign:'center', fontWeight:'bold', color:'white' , fontSize:18}}>0</Text>
+            <Text style={{color:'gray', fontSize:17, marginTop: 6}}>KCAL</Text>
+        </View>
+        <View>
+            <Text style={{textAlign:'center', fontWeight:'bold', color:'white' , fontSize:18}}>0</Text>
+            <Text style={{color:'gray', fontSize:17, marginTop: 6}}>MINS</Text>
+        </View>
       </View>
-    </SafeAreaView>
+      <View style={{justifyContent:'center', alignItems:'center', marginTop:20}}>
+        <Image style={{height:120, width:'85%', borderRadius: 10}} source={{uri: "https://citybook.pk/wp-content/uploads/2019/01/Fitness-Cult-citybook-3.jpg"}} />
+      </View>
+      <FitnessCard/>
+      
+      </View>
+    </ScrollView>
   )
 }
 
