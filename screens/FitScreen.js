215023288054bdb2280f33b97a0c9dbb9649a9e0
MAC <mac@MACs-MacBook-Pro.local>
2022-10-05 03:59:08 -0400

    WorkScreen

--- screens/FitScreen.js
+++ screens/FitScreen.js
@@ -1,4 +1,4 @@
-import { View, Text, SafeAreaView, Image, TouchableOpacity } from 'react-native'
+import { View, Text, SafeAreaView, Image, TouchableOpacity, Pressable } from 'react-native'
 import React, { useState } from 'react'
 import { useNavigation, useRoute } from '@react-navigation/native';
 
@@ -18,9 +18,57 @@ const FitScreen = () => {
         <Text style={{marginLeft:'auto', fontWeight:'300',marginRight: 'auto', marginTop:30, fontSize:30}}>
             x{current.sets}
         </Text>
-        <TouchableOpacity onPress={()=> navigation.navigate('Rest')} style={{backgroundColor:'blue', marginLeft:'auto', marginRight:'auto', marginTop:30, borderRadius:20, padding:10, width:150}}>
-          <Text style={{textAlign:'center', fontWeight:'bold', fontSize:20, color:'white'}}>DONE</Text>
-          </TouchableOpacity>
+        {index + 1 >= excersise.length ? (
+          <TouchableOpacity onPress={()=> {
+            navigation.navigate('Home')
+  
+            setTimeout(()=>{ 
+              setIndex(index+1)
+            })
+          }} style={{backgroundColor:'blue', marginLeft:'auto', marginRight:'auto', marginTop:30, borderRadius:20, padding:10, width:150}}>
+            <Text style={{textAlign:'center', fontWeight:'bold', fontSize:20, color:'white'}}>DONE</Text>
+            </TouchableOpacity>
+
+        ):(
+          <TouchableOpacity onPress={()=> {
+            navigation.navigate('Rest')
+  
+            setTimeout(()=>{
+              setIndex(index+1)
+            })
+          }} style={{backgroundColor:'blue', marginLeft:'auto', marginRight:'auto', marginTop:30, borderRadius:20, padding:10, width:150}}>
+            <Text style={{textAlign:'center', fontWeight:'bold', fontSize:20, color:'white'}}>DONE</Text>
+            </TouchableOpacity>
+
+        )}
+        
+          <View style={{flexDirection:'row', alignItems:'center', marginLeft:'auto', marginRight:'auto', marginTop:50}}>
+          <Pressable disabled={index ===0} onPress={()=> {
+            navigation.navigate('Rest')
+
+            setTimeout(()=>{
+                          setIndex(index-1)
+                        }, 1000)
+          }}style={{backgroundColor:'green', padding:10, borderRadius:20, width:120, marginHorizontal:20}}><Text style={{color:'white', textAlign:'center', fontWeight:'bold'}}>PREV</Text></Pressable>
+          {index + 1 >= excersise.length ? (
+            <TouchableOpacity onPress={()=> {
+              navigation.navigate('Home') 
+    
+              setTimeout(()=>{
+                setIndex(index+1)
+              })
+            }}  style={{backgroundColor:'green', padding:10, borderRadius:20, width:120, marginHorizontal:20}}><Text style={{color:'white', textAlign:'center', fontWeight:'bold'}}>SKIP</Text></TouchableOpacity>
+
+          ):(
+            <TouchableOpacity  onPress={()=> {
+              navigation.navigate('Rest')
+    
+              setTimeout(()=>{
+                setIndex(index+1)
+              })}} style={{backgroundColor:'green', padding:10, borderRadius:20, width:120, marginHorizontal:20}}><Text style={{color:'white', textAlign:'center', fontWeight:'bold'}}>SKIP</Text></TouchableOpacity>
+          )}
+          
+          </View>
     </SafeAreaView>
   )
 }
